4a8c1011491cd3155af173ee4ba8f5c0b488303e
Pavel Shishaev <dartlen@tut.by>
2017-11-13 00:50:27 +0300

    Initial commit

---
+++ app/src/main/java/by/project/dartlen/riversofbelarus/Rivers.java
@@ -0,0 +1,16 @@
+package by.project.dartlen.riversofbelarus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Rivers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rivers);
+
+
+    }
+}
